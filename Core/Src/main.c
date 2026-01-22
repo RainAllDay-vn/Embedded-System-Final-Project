5a207fc456002c4403844de388c2b8145002759f
anhKhoai05 <nguyenvuanhkhoall@gmail.com>
2026-01-22 21:57:16 +0700

    feat:update mario

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -185,6 +185,121 @@ void Buzzer_Stop(void)
 #define C5 523
 #define E5 659
 #define G5 784
+#define C4 262
+#define D4 294
+#define E4 330
+#define F4 349
+#define G4 392
+#define A4 440
+#define B4 494
+
+#define C5 523
+#define D5 587
+#define E5 659
+#define F5 698
+#define G5 784
+#define A5 880
+#define NOTE_E7  2637
+#define NOTE_C7  2093
+#define NOTE_G7  3136
+#define NOTE_G6  1568
+#define NOTE_E6  1319
+#define NOTE_A6  1760
+#define NOTE_B6  1976
+#define NOTE_AS6 1865
+#define NOTE_F7  2794
+#define NOTE_D7  2349
+#define NOTE_A7  3520
+#define REST     0
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
 
 /* ===== STARTUP MELODY ===== */
 void Play_Startup_Melody(void)
@@ -196,19 +311,64 @@ void Play_Startup_Melody(void)
     Buzzer_Stop();
 }
 
-/* ===== MARIO THEME (LOOP) ===== */
+/* ===== MARIO THEME ===== */
 const uint16_t mario_notes[] = {
-    E5, E5, 0, E5, 0, C5, E5, 0,
-    G5, 0, 0, 0,
-    G4, 0, 0, 0
+		NOTE_E7, NOTE_E7, 0, NOTE_E7,
+		  0, NOTE_C7, NOTE_E7, 0,
+		  NOTE_G7, 0, 0, 0,
+		  NOTE_G6, 0, 0, 0,
+
+		  // Theme Section A
+		  NOTE_C7, 0, 0, NOTE_G6,
+		  0, 0, NOTE_E6, 0,
+		  0, NOTE_A6, 0, NOTE_B6,
+		  0, NOTE_AS6, NOTE_A6, 0,
+
+		  NOTE_G6, NOTE_E7, NOTE_G7,
+		  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+		  0, NOTE_E7, 0, NOTE_C7,
+		  NOTE_D7, NOTE_B6, 0, 0,
+
+		  // Repeat Section A
+		  NOTE_C7, 0, 0, NOTE_G6,
+		  0, 0, NOTE_E6, 0,
+		  0, NOTE_A6, 0, NOTE_B6,
+		  0, NOTE_AS6, NOTE_A6, 0,
+
+		  NOTE_G6, NOTE_E7, NOTE_G7,
+		  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+		  0, NOTE_E7, 0, NOTE_C7,
+		  NOTE_D7, NOTE_B6, 0, 0
 };
 
-const uint16_t mario_durations[] = {
-    100,100,100,100,100,100,100,100,
-    300,100,100,100,
-    300,100,100,100
+const uint16_t mario_duration[] = {
+		12, 12, 12, 12,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+
+		  9, 9, 9,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+
+		  9, 9, 9,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12,
+		  12, 12, 12, 12
 };
 
+#define MARIO_LEN (sizeof(mario_notes)/sizeof(uint16_t))
 void MarioMusicTask(void *argument)
 {
     HAL_TIM_PWM_Start(&htim10, TIM_CHANNEL_1);
@@ -218,11 +378,28 @@ void MarioMusicTask(void *argument)
 
     for (;;)
     {
-        Buzzer_SetFrequency(mario_notes[i]);
-        osDelay(mario_durations[i]);
+    	uint16_t note = mario_notes[i];
+    	        uint16_t tempo = mario_duration[i];
 
-        i++;
-        if (i >= len) i = 0;
+    	        uint32_t noteTime = 800 / tempo;   // CHUẨN MARIO
+
+    	        if (note == 0)
+    	        {
+    	            Buzzer_Stop();                  // REST
+    	        }
+    	        else
+    	        {
+    	            Buzzer_SetFrequency(note);
+    	        }
+
+    	        osDelay(noteTime);
+
+    	        // Pause giữa các nốt (30%)
+    	        Buzzer_Stop();
+    	        osDelay(noteTime * 0.3f);
+
+    	        i++;
+    	        if (i >= len) i = 0;
     }
 }
 
